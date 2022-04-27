9dce3002a4e2af046c930effdf4fb9a56eb49d45
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-04-27 09:21:35 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,8 +6,8 @@ console.log(document.domain);
 const image = document.getElementsByTagName("IMG");
 for (let i = 0; i < image.length; i++) {
     image[i].src="https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+    console.log(image[i]);
 }
-console.log(const IMG = document.getElementsByTagName("IMG"); for (let i = 0; i < IMG.length; i++) {return IMG[i];};
 const back = document.getElementsByClassName("infocard-lg-data text-muted");
 for (let i = 0; i < back.length; i++) {
     const back1 = document.getElementsByClassName("itype flying");
